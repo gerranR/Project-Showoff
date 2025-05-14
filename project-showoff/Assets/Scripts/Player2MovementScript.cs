2d8ec9d22f5d9f9fb029ed00d57b1454391d64d6
Gerran <GerranRoord@gmail.com>
2025-05-14 15:18:08 +0200

    fairy dash and dubblejump

--- project-showoff/Assets/Scripts/Player2MovementScript.cs
+++ project-showoff/Assets/Scripts/Player2MovementScript.cs
@@ -5,16 +5,26 @@ using UnityEngine;
 
 public class Player2MovementScript : MonoBehaviour
 {
-    [SerializeField]
-    private float speed;
+    [SerializeField] private float speed;
+    [SerializeField] private float dashForce;
+    [SerializeField] private float LinearDampeningDuringDash;
 
     [SerializeField]
     private Rigidbody2D rb;
 
     private bool isAvectedByGravity = false;
 
-    private ContactPoint2D[] lastContectpoint = new ContactPoint2D[5];
+    private ContactPoint2D[] lastContectpoint = new ContactPoint2D[5] ;
 
+    [SerializeField]
+    private PlayerMovementScript player1;
+
+    [SerializeField]
+    private float attachDistance;
+
+    private bool isAttached;
+    private bool isDashing;
+    private bool doubleJumped;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -25,19 +35,73 @@ public class Player2MovementScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if(Vector3.Distance(transform.position, player1.transform.position) <= attachDistance)
+        {
+            if(Input.GetButtonDown("Attach"))
+            {
+                if(isAttached)
+                {
+                    transform.parent = null;
+                    isAttached = false;
+                }
+                else
+                {
+                    transform.parent = player1.transform;
+                    transform.position = player1.transform.position + new Vector3(1f, 1f,0);
+                    isAttached = true;
+                }
+            }
+        }
+
+        if(Input.GetButtonDown("Dash") && !isDashing)
+        {
+            rb.linearVelocity = rb.linearVelocity.normalized * dashForce * Time.deltaTime;
+            rb.linearDamping = LinearDampeningDuringDash;
+            isDashing = true;
+            
+        }
+
+        if(isAttached && player1.hasJumped())
+        {
+            if(Input.GetButtonDown("DubbleJump") && !doubleJumped)
+            {
+                player1.Jump();
+                doubleJumped = true;
+            }
+        }
+
+        if(doubleJumped && !player1.hasJumped())
+        {
+            doubleJumped = false;
+        }
     }
+
+
     private void FixedUpdate()
     {
-        if (!isAvectedByGravity)
+        if (!isAvectedByGravity && !isAttached)
         {
             Vector2 force = new Vector2(Input.GetAxis("HorizontalP2") * speed * Time.deltaTime, Input.GetAxis("VerticalP2") * speed * Time.deltaTime);
 
-            rb.linearVelocity = force;
+            if (isDashing)
+            {
+                print(rb.linearVelocity);
+                if (rb.linearVelocity.magnitude <= .5f)
+                {
+                    isDashing = false;
+                    rb.linearDamping = 0f;
+                }
+            }
+            else
+            {
+                rb.linearVelocity = force;
+            }
         }
-        //Vector2 pos = transform.position;
-
-        //transform.LookAt(pos + force);
+        else if (isAttached)
+        {
+            rb.linearVelocity = Vector2.zero;
+            transform.position = player1.transform.position + new Vector3(1f, 1f, 0);
+        }
     }
 
     private void OnTriggerStay2D(Collider2D collision)
