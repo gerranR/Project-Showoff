7f7a079f0610021916c856703f3d8ac5af41cc08
Gerran <GerranRoord@gmail.com>
2025-05-15 15:41:35 +0200

    fan / current

---
+++ project-showoff/Assets/Scripts/Current.cs
@@ -0,0 +1,27 @@
+using UnityEngine;
+
+public class Current : MonoBehaviour
+{
+    [SerializeField] float currentSpeed;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        Rigidbody2D rb = collision.GetComponent<Rigidbody2D>();
+        if (rb != null)
+        {
+            rb.AddForce(transform.up * currentSpeed);
+        }
+    }
+}
