22a3d8c862695235cd824ba618de3acce6084697
Gerran <GerranRoord@gmail.com>
2025-05-29 16:13:31 +0200

    cam and throw line

--- project-showoff/Assets/Scripts/CamaraMovement.cs
+++ project-showoff/Assets/Scripts/CamaraMovement.cs
@@ -3,6 +3,10 @@ using UnityEngine;
 public class CamaraMovement : MonoBehaviour
 {
     [SerializeField] GameObject player1;
+    [SerializeField] GameObject player2;
+
+    [SerializeField] float minZoom;
+    [SerializeField] float maxZoom;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -13,6 +17,19 @@ public class CamaraMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        Vector3 pos;
+        pos = (player1.transform.position + player2.transform.position) / 2;
+        pos.z = -Vector3.Distance(player1.transform.position, player2.transform.position);
+
+        if(pos.z > -minZoom)
+        {
+            pos.z = -minZoom;
+        }
+        if(pos.z < -maxZoom)
+        {
+            pos.z = -maxZoom;
+        }
+
+        transform.position = pos;
     }
 }
