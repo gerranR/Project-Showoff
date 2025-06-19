e70bf1589bd044bd5038a9ef92bc851ba2d8d2a6
Gerran <GerranRoord@gmail.com>
2025-06-19 21:04:33 +0200

    pressure plates

--- project-showoff/Assets/Scripts/PlatformButton.cs
+++ project-showoff/Assets/Scripts/PlatformButton.cs
@@ -6,15 +6,25 @@ public class PlatformButton : MonoBehaviour
 {
     [SerializeField] List<GameObject> platforms = new List<GameObject>();
 
+    [SerializeField] Sprite up;
+    [SerializeField] Sprite down;
+    [SerializeField] SpriteRenderer spriteRenderer;
+
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.gameObject.tag == "Human")
+        {
+            spriteRenderer.sprite = down;
             ToggleVisibility();
+        }
     }
     private void OnTriggerExit2D(Collider2D collision)
     {
         if (collision.gameObject.tag == "Human")
+        {
+            spriteRenderer.sprite = up;
             ToggleVisibility();
+        }
     }
     void ToggleVisibility()
     {
